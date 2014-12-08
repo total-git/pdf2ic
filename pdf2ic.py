3acd0110ce22b1f3dd3cb5f2a3565e389063414a
Felix Reihl <felix.reihl@gmail.com>
2014-12-08 11:30:35 +0100

    layout analysis experimentation

--- pdf2ic.py
+++ pdf2ic.py
@@ -1,9 +1,45 @@
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfpage import PDFPage
-from cStringIO import StringIO
+from pdfminer.pdfpage import PDFTextExtractionNotAllowed
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfdevice import PDFDevice
 
+from pdfminer.layout import LAParams
+from pdfminer.converter import PDFPageAggregator
+
+# Open a PDF file.
+fp = open('Iwacu44.pdf', 'rb')
+# Create a PDF parser object associated with the file object.
+parser = PDFParser(fp)
+# Create a PDF document object that stores the document structure.
+# Supply the password for initialization.
+document = PDFDocument(parser, password)
+# Check if the document allows text extraction. If not, abort.
+if not document.is_extractable:
+    raise PDFTextExtractionNotAllowed
+# Create a PDF resource manager object that stores shared resources.
+rsrcmgr = PDFResourceManager()
+# Create a PDF device object.
+device = PDFDevice(rsrcmgr)
+# Create a PDF interpreter object.
+interpreter = PDFPageInterpreter(rsrcmgr, device)
+# Process each page contained in the document.
+for page in PDFPage.create_pages(document):
+    interpreter.process_page(page)
+
+# Set parameters for analysis.
+laparams = LAParams()
+# Create a PDF page aggregator object.
+device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+interpreter = PDFPageInterpreter(rsrcmgr, device)
+for page in PDFPage.create_pages(document):
+    interpreter.process_page(page)
+    # receive the LTPage object for the page.
+    layout = device.get_result()
+
+"""
 def convert_pdf_to_txt(path):
     rsrcmgr = PDFResourceManager()
     retstr = StringIO()
@@ -25,3 +61,4 @@ def convert_pdf_to_txt(path):
     return str
 
 print convert_pdf_to_txt("../embedded_text/sample_art.pdf")
+"""
