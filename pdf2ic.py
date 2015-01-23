b143530c5b9b3a5713d8712efe14c4057263e3e8
Felix Reihl <felix.reihl@gmail.com>
2015-01-23 18:16:59 +0100

    fixed some encoding issue

--- pdf2ic.py
+++ pdf2ic.py
@@ -8,6 +8,7 @@ from pdfminer.layout import LAParams
 
 import sys, getopt
 import re
+import codecs
 
 #import xml.etree.cElementTree as ET
 #import elementtree.ElementTree as ET
@@ -20,6 +21,7 @@ def most_common(lst):
     return max(set(lst), key=lst.count)
 
 def main(argv):
+    # command line options
     infile  = ''
     outfile = ''
     password = ''
@@ -47,11 +49,14 @@ def main(argv):
         infile = args[0]
         if not outfile:
             outfile = '.'.join(infile.split('.')[:-1] + ['csv'])
+
     # set up a locale object from the language
     loc = locale(language)
 
-    # convert the pdf to an xml file
+    # build the basename of the xml file (foo.pdf -> foo.xml)
     xml_filename = '.'.join(infile.split('.')[:-1] + ['xml'])
+
+    # convert the pdf to an xml file
     '''
     with open(xml_filename, 'w') as xmlfd1:
         rsrcmgr = PDFResourceManager()
@@ -62,52 +67,73 @@ def main(argv):
             for page in PDFPage.get_pages(pdffd):
                 # page.rotate = (page.rotate+rotation) % 360
                 interpreter.process_page(page)
+
     # for some reason, xmlconverter forgets the closing </pages> tag
     with open(xml_filename, 'a') as xmlfd:
         xmlfd.write('</pages>')
     '''
 
+    # will store the dates; the most commom one will be assumed to be the publishing date of the paper
+    dates = []
+    weekday = u''
+    day = u''
+    month = u''
+    year = u''
+
+    current_text = [] # String list because otherwise python will copy the string each time we concatenate something to it
+    articles = []
+
+    first_letter = u''
+
+    fontsizes = {} # a dictionary for the used font sizes; will be sorted to give the most common font size
+    fontnames = {} # a dictionary for the used fonts
+    prev_fontsize = 0
+    prev_fontname = u''
+
+    complete_text = [] # List of tuples containing the text and its size
+
     # parse the xml file
     tree = etree.parse(xml_filename)
     doc = tree.getroot()
-    context = etree.iterparse(xml_filename, tag='text')
-    fontsizes = {}
-    articles = []
-    dates = []
-    weekday = ''
-    day = ''
-    month = ''
-    year = ''
-    current_text = [] # String list because otherwise python will copy the string each time we concatenate something to it
-    prev_fontsize = 0
-    prev_fontname = ""
-    complete_text = [] # List of tuples containing the text and its size
+    context = etree.iterparse(xml_filename, tag=u'text')
     for action, elem in context:
         if elem.text:
-            if elem.attrib['size'] in fontsizes:
-                fontsizes[elem.attrib['size']] += 1
+            if elem.attrib[u'size'] in fontsizes:
+                fontsizes[elem.attrib[u'size']] += 1
+                fontnames[elem.attrib[u'font']] += 1
             else:
-                fontsizes[elem.attrib['size']] = 1
-    textsize = sorted(fontsizes, key=fontsizes.__getitem__)[-1] # the most common font size is probably the one for the article text
+                fontsizes[elem.attrib[u'size']] = 1
+                fontnames[elem.attrib[u'font']] = 1
+    article_fontsize = sorted(fontsizes, key=fontsizes.__getitem__)[-1] # the most common font size is probably the one for the article text
+    '''
+    print sorted(fontsizes, key=fontsizes.__getitem__)
+    print fontsizes
+    for i in sorted(fontsizes, key=fontsizes.__getitem__):
+        print i, fontsizes[i]
+    for i in sorted(fontnames, key=fontnames.__getitem__):
+        print i, fontnames[i]
+    '''
     root = context.root
-    context = etree.iterwalk(root, tag='text')
-    # write all text snippets that have the same font size to the text list together with their font size
+    context = etree.iterwalk(root, tag=u'text') # iterwalk does the same as iterparse, but uses the in-memory tree
+
+    # write all text snippets that have the same font size to the text list (together with their font size)
     for action, elem in context:
         if elem.text:
-            if elem.attrib['size'] == prev_fontsize and elem.attrib['font'] == prev_fontname:
+            if elem.attrib[u'size'] == prev_fontsize and elem.attrib[u'font'] == prev_fontname:
                 current_text.append(elem.text)
             else:
-                complete_text.append((''.join(current_text), elem.attrib['size'], elem.attrib['font']))
+                complete_text.append((u''.join(current_text), elem.attrib[u'size'], elem.attrib[u'font']))
                 current_text = [elem.text]
-            prev_fontsize = elem.attrib['size']
-            prev_fontname = elem.attrib['font']
+            prev_fontsize = elem.attrib[u'size']
+            prev_fontname = elem.attrib[u'font']
+    # iterate over the text segments
     for text, size, font in complete_text:
         # find out the date
         tmpdate = text.split()
-        tmp = re.findall(r'(?<=\s)\d{4}(?=\s)', text)
+        tmp = re.findall(u'(?<=\s)\d{4}(?=\s)', text)
         if tmp:
             year = tmp[0]
-        tmp = re.findall(r'(?<=\s)\d{2}(?=\s)', text)
+        tmp = re.findall(u'(?<=\s)\d{2}(?=\s)', text)
         if tmp:
             day = tmp[0]
         for i in tmpdate:
@@ -115,12 +141,26 @@ def main(argv):
                 weekday = loc.weekdays[i.lower()]
             if i.lower() in loc.months:
                 month = loc.months[i.lower()]
-        dates.append(year+'-'+month+'-'+day+'-'+weekday)
+        dates.append(year+u'-'+month+u'-'+day+u'-'+weekday) # append it to the date list
 
         # find out the current category
         if text.lower() in loc.categories:
             category = text
-    # take the most common day and date
+
+        # find 1-Letter strings (first letter of an article is commonly in a larger font)
+        if len(text) <= 2:
+            first_letter = text # if what follows is an article starting with a lowercase letter, append it to the beginning
+
+        
+        if float(size) == article_size:
+            print codecs.encode(text, 'utf-8')
+            #if text:
+                #print text
+                #if text[0].islower():
+                    #article_text = first_letter + text
+                    #print article_text
+
+    # use the most common date
     date = most_common(dates)
 
     '''
@@ -132,3 +172,24 @@ def main(argv):
 
 if __name__ == '__main__':
     main(sys.argv[1:])
+
+
+# TODO
+'''
+replace ﬁ with fi (and reconcatenate the words like "ﬁ n")
+potentially the same problem with
+ﬀ -> ff
+ﬂ -> fl
+ﬃ -> ffi
+ﬄ -> ffl
+
+line endings
+
+hebrew weekdays, months and categories
+
+potentially other languages
+
+append the first letter of an article (because it is in another font size)
+
+delete xml file afterwards
+'''
